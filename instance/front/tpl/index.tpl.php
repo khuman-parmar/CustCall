0b8e8cf6b0a1968c86fb8654a10eabd01d11cea8
Khuman-Parmar <kp67869@gmail.com>
2014-12-30 19:06:47 +0530

    Cust

--- instance/front/tpl/index.tpl.php
+++ instance/front/tpl/index.tpl.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>Endless Admin</title>
+        <title>Cust Call</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
         <meta name="author" content="">
